ca5d7181ea58549d59a35cd93d9b925efc1bf50d
Estela Alves. <estela.almoraes@gmail.com>
2025-06-16 19:26:39 -0300

    inicio das outras páginas

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,15 @@
 
 
 function navigate(location) {
+    if(location == 'previous'){
+        history.back();
+    }else{
     window.location.href = location;
+
+    }
 }
 
+function isLogged(){
+    let isLogged = localStorage.getItem('login') 
+    return isLogged;
+}
