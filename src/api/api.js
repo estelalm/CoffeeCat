d07fa4b4fc531105ad501e7d8ec6d9fc1bb08b08
Estela Alves. <estela.almoraes@gmail.com>
2025-07-07 07:02:04 -0300

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,13 @@
+const BASE_URL = "https://coffee-cat.onrender.com/1.0/cofeecat/";
 
-const BASE_URL = "localhost...../";
 
 
-const getCardapio = () =>{
+export const getCardapio = async () => {
+  let url = `${BASE_URL}produtos`;
+  const response = await fetch(url);
+  console.log(response)
+  const data = await response.json();
+  console.log(data)
+  return data.produtos;
+};
 
-    let url = `${BASE_URL}produtos`
-    
-}
